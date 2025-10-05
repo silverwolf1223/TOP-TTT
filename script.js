d84e7b57d5ed35f22b08096c420da5a9a09e64d4
silverwolf1223 <101886723+silverwolf1223@users.noreply.github.com>
2025-10-04 19:03:05 -0500

    turn switching and reset button

--- script.js
+++ script.js
@@ -29,23 +29,18 @@ let player2 = Player('Player2', 'O');
 
 const gameBoard = function(){
     let Board = Array.from(DOMmanager.BoardArray).map(item => item.textContent);
-    let BoardObj = {
-    line1 : [Board[0], Board[1], Board[2]],
-    line2 : [Board[3], Board[4], Board[5]],
-    line3 : [Board[6], Board[7], Board[8]],
-    column1 : [Board[0], Board[3], Board[6]],
-    column2 : [Board[1], Board[4], Board[7]],
-    column3 : [Board[2], Board[5], Board[8]],
-    diagonal1 : [Board[0], Board[4], Board[8]],
-    diagonal2 : [Board[6], Board[4], Board[2]],
-    };
+    let BoardObj;
+
+    let gameWon = false;
 
     const checkWin = function(){
         UpdateBoard();
         Object.keys(BoardObj).forEach(obj =>{
             if(BoardObj[obj][0] == BoardObj[obj][1] && BoardObj[obj][1] == BoardObj[obj][2] && BoardObj[obj][0] != '')
             {
-                return console.log('You win!');
+                DOMmanager.playerTurn.score += 1;
+                let gameWon = true;
+                return console.log(`${DOMmanager.playerTurn.name} wins!`);
             }
         })
     }
@@ -71,33 +66,62 @@ const gameBoard = function(){
     };
 };
 
-const game = (function(){
-    let Board = new Array(9);
-
-    const startGame = (function(){
-        Board = new Array(9);
-        console.log(`Welcome ${player1.name} and ${player2.name}`)
-        console.log(`${player1.name} score: ${player1.score}, ${player2.name} score: ${player2.score}`)
-    });
-
-    startGame();
-
-    return {startGame, Board};
-})();
-
 const DOMmanager = (function(doc){
     let screenBoard = doc.querySelectorAll('td');
     let BoardArray = Array.from(screenBoard);
-    let playerTrun = (!player1) ? player1: player2;
+    let p1t = true;
+    let playerTurn = (p1t) ? player1: player2;
 
-    screenBoard.forEach(i => i.addEventListener('click', function(){i.innerHTML = playerTrun.marker;
-        gameBoard().checkWin();
-        screenBoard = doc.querySelectorAll('td');
-        BoardArray = Array.from(screenBoard);
+    const turnReader = doc.querySelector('#turn');
+
+    const body = doc.querySelector('h1');
+
+    body.addEventListener('click', function(){
+        if(gameBoard.gameWon)
+        {
+            startGame();
+            gameBoard.gameWon = false;
+        }
+    })
+
+    screenBoard.forEach(i => i.addEventListener('click', function(){
+        if(i.textContent == '')
+        {
+            i.innerHTML = playerTurn.marker;
+            gameBoard().checkWin();
+            p1t = !p1t;
+            playerTurn = (p1t) ? player1: player2;
+            screenBoard = doc.querySelectorAll('td');
+            BoardArray = Array.from(screenBoard);
+            turnUpdate();
+        }
     }))
+
+    const turnUpdate = function(){
+        turnReader.innerHTML = playerTurn.name
+    }
+
+
+    turnUpdate();
 
     return {
         screenBoard,
         BoardArray,
+        playerTurn,
     }
 })(document || documentMock);
+
+const game = (function(){
+    let Board = new Array(9);
+    console.log(`Welcome ${player1.name} and ${player2.name}`);
+
+    const startGame = (function(){
+        Board = new Array(9);
+        (DOMmanager.BoardArray).map(item => item.textContent = '');
+        console.log(`${player1.name} score: ${player1.score}, ${player2.name} score: ${player2.score}`);
+    });
+
+    startGame();
+
+    return {startGame, Board,};
+})();
